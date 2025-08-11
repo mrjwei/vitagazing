650506d84876b8a357f8829f14236bd6844d44ea
Jesse Wei <jesseweijapan@gmail.com>
2025-08-12 09:50:54 +1000

    DRB-36 modify handlers to handle added work experiences field

--- backend/controllers/resuemController.js
+++ backend/controllers/resuemController.js
@@ -1,7 +1,10 @@
 const Resume = require("../models/Resume")
 
 const createResume = async (req, res) => {
-  const { firstname, lastname, email, phone } = req.body
+  const { firstname, lastname, email, phone, summary, workExperiences = [] } = req.body
+    const stripIds = arr => Array.isArray(arr)
+    ? arr.map(({ id, ...rest }) => rest)
+    : [];
   try {
     const resume = await Resume.create({
       userId: req.user.id,
@@ -9,6 +12,8 @@ const createResume = async (req, res) => {
       lastname,
       email,
       phone,
+      summary,
+      workExperiences: stripIds(workExperiences),
     })
     res.status(201).json(resume)
   } catch (error) {
@@ -26,7 +31,7 @@ const fetchResumes = async (req, res) => {
 }
 
 const updateResume = async (req, res) => {
-  const { firstname, lastname, email, phone } = req.body
+  const { firstname, lastname, email, phone, summary, workExperiences = [] } = req.body
   try {
     const resume = await Resume.findById(req.params.id)
     if (!resume) {
@@ -36,6 +41,8 @@ const updateResume = async (req, res) => {
     resume.lastname = lastname || resume.lastname
     resume.email = email || resume.email
     resume.phone = phone || resume.phone
+    resume.summary = summary || resume.summary
+    resume.workExperiences = workExperiences || resume.workExperiences
     const updatedResume = await resume.save()
     res.json(updatedResume)
   } catch (error) {
--- backend/models/Resume.js
+++ backend/models/Resume.js
@@ -1,12 +1,21 @@
 
 const mongoose = require('mongoose');
 
+const workExperienceSchema = new mongoose.Schema({
+    companyName: { type: String, required: true },
+    startDate: { type: String, required: true },
+    endDate: { type: String, required: true },
+    responsibility: { type: String, required: true }
+}, { _id: false });
+
 const resumeSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
+    summary: {type: String, required: true},
+    workExperiences: {type: [workExperienceSchema], default: []},
 });
 
 module.exports = mongoose.model('Resume', resumeSchema);
