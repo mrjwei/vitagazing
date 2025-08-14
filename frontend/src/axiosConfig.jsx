3888f7b93dbae8d192597b661a3d5ce8dc3a26ad
Jesse Wei <jesseweijapan@gmail.com>
2025-08-14 10:43:15 +1000

    chore: change base url

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.55.217.186:5001', // live
+  baseURL: 'http://13.239.239.125:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
