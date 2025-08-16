69189b7f920ff1dac2ce9e5a1ee7987d11f0c1b7
Jesse Wei <jesseweijapan@gmail.com>
2025-08-16 22:10:53 +1000

    DRB-89 test resume fetching

--- backend/test/test.js
+++ backend/test/test.js
@@ -87,4 +87,194 @@ describe("Resume creation", () => {
     stub.restore()
   })
 })
+
+describe("Resume fetching", () => {
+  it("should return all resumes of a user", async () => {
+    const resumes = [
+      {
+        firstname: "John",
+        lastname: "Doe",
+        email: "john@mail.com",
+        phone: "0000111222",
+        summary: "This is a sample summary.",
+        workExperiences: [
+          {
+            companyName: "Sample Company 1",
+            startDate: "2020-01-20",
+            endDate: "2022-10-30",
+            responsibility: "Sample responsibility description",
+          },
+          {
+            companyName: "Sample Company 2",
+            startDate: "2023-02-01",
+            endDate: "2025-07-31",
+            responsibility: "Sample responsibility description 2",
+          },
+        ],
+        educations: [
+          {
+            degree: "Bachelor of Engineering",
+            institution: "The University of Queensland",
+            startDate: "2010-02-01",
+            endDate: "2014-01-31",
+          },
+          {
+            degree: "Master of IT",
+            institution: "Queensland University of Technology",
+            startDate: "2014-07-01",
+            endDate: "2015-06-30",
+          },
+        ],
+        template: "Software Engineer",
+      },
+      {
+        firstname: "Alice",
+        lastname: "Wong",
+        email: "alice@mail.com",
+        phone: "6666777888",
+        summary: "This is another sample summary.",
+        workExperiences: [
+          {
+            companyName: "Fake Company 1",
+            startDate: "2020-01-20",
+            endDate: "2022-10-30",
+            responsibility: "Fake responsibility description",
+          },
+          {
+            companyName: "Fake Company 2",
+            startDate: "2023-02-01",
+            endDate: "2025-07-31",
+            responsibility: "Fake responsibility description 2",
+          },
+        ],
+        educations: [
+          {
+            degree: "Bachelor of Education",
+            institution: "The University of Queensland",
+            startDate: "2011-11-01",
+            endDate: "2015-10-31",
+          },
+          {
+            degree: "Master of Design",
+            institution: "Queensland University of Technology",
+            startDate: "2016-01-01",
+            endDate: "2016-12-31",
+          },
+        ],
+        template: "Designer",
+      },
+    ]
+    const req = {
+      user: { id: new mongoose.Types.ObjectId() },
+    }
+    const res = {
+      json: sinon.spy(),
+    }
+    const stub = sinon.stub(Resume, "find").resolves(resumes)
+
+    await fetchResumes(req, res)
+
+    expect(stub.calledOnceWith({ userId: req.user.id })).to.be.true
+    expect(res.json.calledWith(resumes)).to.be.true
+
+    stub.restore()
+  })
+
+  it("should be able to return a single resume of a user", async () => {
+    const req = {
+      user: { id: new mongoose.Types.ObjectId() },
+      params: { id: new mongoose.Types.ObjectId() },
+    }
+    const res = {
+      json: sinon.spy(),
+    }
+    const resume = {
+      firstname: "John",
+      lastname: "Doe",
+      email: "john@mail.com",
+      phone: "0000111222",
+      summary: "This is a sample summary.",
+      workExperiences: [
+        {
+          companyName: "Sample Company 1",
+          startDate: "2020-01-20",
+          endDate: "2022-10-30",
+          responsibility: "Sample responsibility description",
+        },
+        {
+          companyName: "Sample Company 2",
+          startDate: "2023-02-01",
+          endDate: "2025-07-31",
+          responsibility: "Sample responsibility description 2",
+        },
+      ],
+      educations: [
+        {
+          degree: "Bachelor of Engineering",
+          institution: "The University of Queensland",
+          startDate: "2010-02-01",
+          endDate: "2014-01-31",
+        },
+        {
+          degree: "Master of IT",
+          institution: "Queensland University of Technology",
+          startDate: "2014-07-01",
+          endDate: "2015-06-30",
+        },
+      ],
+      template: "Software Engineer",
+    }
+    const stub = sinon.stub(Resume, "findOne").resolves(resume)
+
+    await fetchResume(req, res)
+
+    expect(stub.calledOnceWith({ _id: req.params.id, userId: req.user.id })).to
+      .be.true
+    expect(res.json.calledWith(resume)).to.be.true
+
+    stub.restore()
+  })
+
+  it("should return 404 if the resume was not found", async () => {
+    const req = {
+      user: { id: new mongoose.Types.ObjectId() },
+      params: { id: new mongoose.Types.ObjectId() },
+    }
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    }
+    const resume = null
+
+    const stub = sinon.stub(Resume, "findOne").resolves(resume)
+
+    await fetchResume(req, res)
+
+    expect(res.status.calledWith(404)).to.be.true
+    expect(res.json.calledWith({ message: "Resume not found" })).to.be.true
+
+    stub.restore()
+  })
+
+  it("should return 500 if an error other than not-found occurs", async () => {
+    const req = {
+      user: { id: new mongoose.Types.ObjectId() },
+      params: { id: new mongoose.Types.ObjectId() },
+    }
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    }
+
+    const stub = sinon.stub(Resume, "findOne").throws(new Error("Server Error"))
+
+    await fetchResume(req, res)
+
+    expect(res.status.calledWith(500)).to.be.true
+    expect(res.json.calledWith({ message: "Server Error" })).to.be.true
+
+    stub.restore()
+  })
+})
+
 })
