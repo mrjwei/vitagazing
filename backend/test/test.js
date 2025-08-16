9170b8c33df669f296f905efa3333f20fce7020a
Jesse Wei <jesseweijapan@gmail.com>
2025-08-16 22:12:13 +1000

    DRB-88 test resume update

--- backend/test/test.js
+++ backend/test/test.js
@@ -277,4 +277,264 @@ describe("Resume fetching", () => {
   })
 })
 
+describe("Resume updating", () => {
+  it("should update a resume successfully", async () => {
+    const resume = {
+      firstname: "John",
+      lastname: "Doe",
+      email: "john@mail.com",
+      phone: "0000111222",
+      summary: "This is a sample summary.",
+      workExperiences: [
+        {
+          companyName: "Sample Company 1",
+          startDate: "2020-01-20",
+          endDate: "2022-10-30",
+          responsibility: "Sample responsibility description",
+        },
+        {
+          companyName: "Sample Company 2",
+          startDate: "2023-02-01",
+          endDate: "2025-07-31",
+          responsibility: "Sample responsibility description 2",
+        },
+      ],
+      educations: [
+        {
+          degree: "Bachelor of Engineering",
+          institution: "The University of Queensland",
+          startDate: "2010-02-01",
+          endDate: "2014-01-31",
+        },
+        {
+          degree: "Master of IT",
+          institution: "Queensland University of Technology",
+          startDate: "2014-07-01",
+          endDate: "2015-06-30",
+        },
+      ],
+      template: "Software Engineer",
+      save: sinon.stub().returnsThis(),
+    }
+    const req = {
+      body: {
+        ...resume,
+        firstname: "Alice",
+        workExperiences: [
+          ...resume.workExperiences,
+          {
+            companyName: "Sample Company 3",
+            startDate: "2025-08-01",
+            endDate: "2025-08-11",
+            responsibility: "Sample responsibility description 3",
+          },
+        ],
+        template: "Designer"
+      },
+      params: { id: new mongoose.Types.ObjectId() },
+    }
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    }
+    const stub = sinon.stub(Resume, "findById").resolves(resume)
+
+    await updateResume(req, res)
+
+    expect(stub.calledOnceWith(req.params.id)).to.be.true
+    expect(resume.save.calledOnce).to.be.true
+    expect(resume.firstname).to.equal(req.body.firstname)
+    expect(resume.workExperiences).to.deep.equal(req.body.workExperiences)
+    expect(resume.template).to.equal(req.body.template)
+    expect(res.json.calledWith({ ...req.body }))
+
+    stub.restore()
+  })
+
+  it("should return 404 if the resume was not found", async () => {
+    const resume = {
+      firstname: "John",
+      lastname: "Doe",
+      email: "john@mail.com",
+      phone: "0000111222",
+      summary: "This is a sample summary.",
+      workExperiences: [
+        {
+          companyName: "Sample Company 1",
+          startDate: "2020-01-20",
+          endDate: "2022-10-30",
+          responsibility: "Sample responsibility description",
+        },
+        {
+          companyName: "Sample Company 2",
+          startDate: "2023-02-01",
+          endDate: "2025-07-31",
+          responsibility: "Sample responsibility description 2",
+        },
+      ],
+      educations: [
+        {
+          degree: "Bachelor of Engineering",
+          institution: "The University of Queensland",
+          startDate: "2010-02-01",
+          endDate: "2014-01-31",
+        },
+        {
+          degree: "Master of IT",
+          institution: "Queensland University of Technology",
+          startDate: "2014-07-01",
+          endDate: "2015-06-30",
+        },
+      ],
+      template: "Software Engineer",
+      save: sinon.stub().returnsThis(),
+    }
+    const req = {
+      body: {
+        ...resume,
+        firstname: "Alice",
+        workExperiences: [
+          ...resume.workExperiences,
+          {
+            companyName: "Sample Company 3",
+            startDate: "2025-08-01",
+            endDate: "2025-08-11",
+            responsibility: "Sample responsibility description 3",
+          },
+        ],
+      },
+      params: { id: new mongoose.Types.ObjectId() },
+    }
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    }
+    const nullResume = null
+
+    const stub = sinon.stub(Resume, "findById").resolves(nullResume)
+
+    await updateResume(req, res)
+
+    expect(stub.calledOnceWith(req.params.id)).to.be.true
+    expect(res.status.calledWith(404)).to.be.true
+    expect(res.json.calledWith({ message: "Resume not found" })).to.be.true
+
+    stub.restore()
+  })
+
+  it("should return 500 if an error other than not-found occurs when trying to find the resume", async () => {
+    const resume = {
+      firstname: "John",
+      lastname: "Doe",
+      email: "john@mail.com",
+      phone: "0000111222",
+      summary: "This is a sample summary.",
+      workExperiences: [
+        {
+          companyName: "Sample Company 1",
+          startDate: "2020-01-20",
+          endDate: "2022-10-30",
+          responsibility: "Sample responsibility description",
+        },
+        {
+          companyName: "Sample Company 2",
+          startDate: "2023-02-01",
+          endDate: "2025-07-31",
+          responsibility: "Sample responsibility description 2",
+        },
+      ],
+      educations: [
+        {
+          degree: "Bachelor of Engineering",
+          institution: "The University of Queensland",
+          startDate: "2010-02-01",
+          endDate: "2014-01-31",
+        },
+        {
+          degree: "Master of IT",
+          institution: "Queensland University of Technology",
+          startDate: "2014-07-01",
+          endDate: "2015-06-30",
+        },
+      ],
+      template: "Software Engineer",
+      save: sinon.stub().returnsThis(),
+    }
+    const req = {
+      body: resume,
+      params: { id: new mongoose.Types.ObjectId() },
+    }
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    }
+
+    const stub = sinon.stub(Resume, "findById").throws(new Error('An unknown error occurred'))
+
+    await updateResume(req, res)
+
+    expect(res.status.calledWith(500)).to.be.true
+    expect(res.json.calledWith({ message: 'An unknown error occurred' })).to.be.true
+
+    stub.restore()
+  })
+
+  it("should return 500 if an error other than not-found occurs when trying to save", async () => {
+    const resume = {
+      firstname: "John",
+      lastname: "Doe",
+      email: "john@mail.com",
+      phone: "0000111222",
+      summary: "This is a sample summary.",
+      workExperiences: [
+        {
+          companyName: "Sample Company 1",
+          startDate: "2020-01-20",
+          endDate: "2022-10-30",
+          responsibility: "Sample responsibility description",
+        },
+        {
+          companyName: "Sample Company 2",
+          startDate: "2023-02-01",
+          endDate: "2025-07-31",
+          responsibility: "Sample responsibility description 2",
+        },
+      ],
+      educations: [
+        {
+          degree: "Bachelor of Engineering",
+          institution: "The University of Queensland",
+          startDate: "2010-02-01",
+          endDate: "2014-01-31",
+        },
+        {
+          degree: "Master of IT",
+          institution: "Queensland University of Technology",
+          startDate: "2014-07-01",
+          endDate: "2015-06-30",
+        },
+      ],
+      template: "Software Engineer",
+      save: sinon.stub().throws(new Error('Failed to save')),
+    }
+    const req = {
+      body: resume,
+      params: { id: new mongoose.Types.ObjectId() },
+    }
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    }
+
+    const stub = sinon.stub(Resume, "findById").resolves(resume)
+
+    await updateResume(req, res)
+
+    expect(res.status.calledWith(500)).to.be.true
+    expect(res.json.calledWith({ message: 'Failed to save' })).to.be.true
+
+    stub.restore()
+  })
+})
+
 })
