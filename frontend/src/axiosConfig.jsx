37c1931f3a5e49bddebdd286e09e4551429c56f1
Jesse Wei <jesseweijapan@gmail.com>
2025-09-20 15:45:12 +1000

    update baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.107.178.78:5001', // live
+  baseURL: 'http://13.236.119.182:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
