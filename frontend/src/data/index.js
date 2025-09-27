d560a6c442f2c03f857941e01569570812a286d9
Jesse Wei <jesseweijapan@gmail.com>
2025-09-27 15:52:08 +1000

    VIT-30 Story 4-4: Refactor code

--- frontend/src/data/index.js
+++ frontend/src/data/index.js
@@ -1,13 +1,7 @@
 import { DefaultTemplate, DesignerTemplate, SoftwareEngineerTemplate } from "../components/resume-templates"
 
-export const templates = {
-  'Default': {
-    component: DefaultTemplate
-  },
-  'Designer': {
-    component: DesignerTemplate
-  },
-  'Software Engineer': {
-    component: SoftwareEngineerTemplate
-  }
-}
+export const templates = [
+  {id: "default", name: "Default", component: DefaultTemplate},
+  {id: "designer", name: "Designer", component: DesignerTemplate},
+  {id: "software_engineer", name: "Software Engineer", component: SoftwareEngineerTemplate}
+]
