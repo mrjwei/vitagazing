c7de8366f11bbfcaaefb2a2af608cc05889f1e78
Jesse Wei <jesseweijapan@gmail.com>
2025-10-03 18:27:30 +1000

    fix: routes

--- backend/routes/jobBoardRoutes.js
+++ backend/routes/jobBoardRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const JobBoardController = require('../controllers/jobBoard');
-const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
+const { AuthMiddleware } = require('../middleware/authMiddleware');
+
+const authMiddleware = new AuthMiddleware();
 
 const jobBoardController = new JobBoardController();
 
-router.route('/').get(protect, jobBoardController.fetchAll.bind(jobBoardController)).post(protect, jobBoardController.create.bind(jobBoardController));
-router.route('/:id').get(protect, jobBoardController.fetchOne.bind(jobBoardController)).put(protect, jobBoardController.update.bind(jobBoardController)).delete(protect, jobBoardController.delete.bind(jobBoardController));
+router.route('/').get(authMiddleware.handle, jobBoardController.fetchAll.bind(jobBoardController)).post(authMiddleware.handle, jobBoardController.create.bind(jobBoardController));
+router.route('/:id').get(authMiddleware.handle, jobBoardController.fetchOne.bind(jobBoardController)).put(authMiddleware.handle, jobBoardController.update.bind(jobBoardController)).delete(authMiddleware.handle, jobBoardController.delete.bind(jobBoardController));
 
 module.exports = router;
 
